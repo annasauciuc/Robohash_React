81e5814807a63a0a2dda6a2e04f0c1b3d5049d87
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-14 01:09:22 +0000

    adding function to filter the robots

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,16 +1,15 @@
 import React from 'react';
 const Card = (props) => {
-        const {name,email, id,ana} = props;
+        const {name,email, id} = props;
     return (
 
         <div className = "tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-        <img alt = "robot" src ={`https://robohash.org/${id}?200x200`}/>
-        <div>
-        <h2>{name}</h2>
-         <p>{email}</p>
-    
-          </div> 
-          </div>
+           <img alt = "robot" src ={`https://robohash.org/${id}?200x200`}/>
+            <div>
+           <h2>{name}</h2>
+           <p>{email}</p>
+           </div> 
+        </div>
     );
 }
 
