3d4561ad8794ee3184dda9e1388c3a3f12761f3e
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-14 16:50:54 +0000

    adding scroll component

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-
+import Scroll from "./Scroll";
 
 import './App.css';
 
@@ -32,7 +32,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1 green sans-serif   mt5 '>Best Friends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
             <CardList animals={filteredanimals} />
+            </Scroll>
         </div>
       );
   }
---
+++ robofriends/src/Scroll.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Scroll = (props) => {
+    return (
+        <div style={{overflowY:'scroll',  height:'1000px'}}>{props.children}</div>
+    ) 
+}
+export default Scroll;
