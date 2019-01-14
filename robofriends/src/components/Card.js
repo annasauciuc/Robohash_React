32410a0dd2a46b4f654c788b40812dc139e531e3
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-14 17:14:06 +0000

    organize the components folder and containers

--- robofriends/src/components/Card.js
+++ robofriends/src/components/Card.js
@@ -1,16 +1,14 @@
 import React from 'react';
 const Card = (props) => {
-        const {name,email, id} = props;
+    const { name, email, id } = props;
     return (
 
-        <div className = "tc bg-green dib br3 pa3 ma2 grow bw2 shadow-5">
-           {/* <img alt = "robot" src ={`http://i.pravatar.cc/350x150?${id}`}/> */}
-         <a href="https://en.wikipedia.org/wiki/Cat" target="_blank" >  <img alt = "image of a beautiful cat" src ={`https://loremflickr.com/320/240?random=${id}`}/></a>
-           
-            <div>
-           <h2>{name}</h2>
-           <p>{email}</p>
-           </div> 
+        <div className = "tc bg-green dib br3 pa3 ma2 grow bw2 shadow-5" > { /* <img alt = "robot" src ={`http://i.pravatar.cc/350x150?${id}`}/> */ }
+         <a href = "https://en.wikipedia.org/wiki/Cat" target = "_blank" > <img alt = "image of a beautiful cat" src = {`https://loremflickr.com/320/240?random=${id}` }/></a>
+          <div>
+        <h2> { name } </h2> 
+        <p> { email } </p>
+         </div> 
         </div>
     );
 }
