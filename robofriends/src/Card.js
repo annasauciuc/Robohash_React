54355ea5bfa9aaeb66be59561c518fbcb4cf26d7
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-14 15:51:04 +0000

    changing robots to animals

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -4,7 +4,9 @@ const Card = (props) => {
     return (
 
         <div className = "tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-           <img alt = "robot" src ={`https://robohash.org/${id}?200x200`}/>
+           {/* <img alt = "robot" src ={`http://i.pravatar.cc/350x150?${id}`}/> */}
+           <img alt = "robot" src ={`https://loremflickr.com/320/240?random=${id}`}/>
+           
             <div>
            <h2>{name}</h2>
            <p>{email}</p>
