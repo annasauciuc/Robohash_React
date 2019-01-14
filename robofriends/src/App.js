f967afa4866597635943a873a2bb3af71313ad15
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-14 12:55:55 +0000

    fixing the error in the call to api

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,54 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 
+
 import './App.css';
 
-
-class  App extends Component  {
-    constructor(){
-        super()
-        this.state={
-            robots:[],
-            searchField:''
-        }
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
     }
-componentDidMount(){
+  }
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response =>{
-        console.log('response', response.json())
-        response.json();
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    .then(users=>{
-        this.setState({robots:users})
-    })
-}
-
-
-    onSearchChange = (event) => {
-        this.setState({searchField:event.target.value});
-
-    
-    }
-
-
-    render(){
-        const filteredRobots = this.state.robots.filter(robots => {
-        return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-        })
-if(this.state.robots.length === 0){
-    return <h1>Loading</h1>
-}     else{
-    return(
-        <div className="tc">
-            <h1 className="f2">RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <CardList robots={filteredRobots}/>
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots} />
         </div>
-        
-    );
-     }
-   }
- 
+      );
+  }
 }
+
 export default App;
