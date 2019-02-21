a847c0b9fbafc2badf71dc8a70489bdaec04dcea
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-02-21 12:45:39 +0000

    adding redux to the app , redux store and provider

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
+import "./index.css";
 
-import App from './containers/App'
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
-import { animals } from './animals';
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { animals } from "./animals";
 
-ReactDOM.render( < App / > ,
-    document.getElementById('root'));
+import { searchRobots } from "./reducers";
+
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App  />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
